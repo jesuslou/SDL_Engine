d7821b625e627d2f028c48d3a19fa929a5c07d42
jesuslou <loucocito@gmail.com>
2015-07-22 19:56:57 +0200

    - glm sources added

---
+++ source/SDL_engine/common.h
@@ -0,0 +1,13 @@
+#ifndef _INC_COMMON_H_
+#define _INC_COMMON_H_
+
+#include <string>
+#include <glm/glm.hpp>
+
+#define TPoint2     glm::vec2
+#define TPoint3     glm::vec3
+#define TPoint4     glm::vec4
+#define TQuaternion glm::quat
+#define TMatrix     glm::mat4
+
+#endif
