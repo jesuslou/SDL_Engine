cf3918e0316ceaf1f54aeb838d23688311c7e298
jesuslou <loucocito@gmail.com>
2015-07-29 01:31:40 +0200

    - sdl_rect constructors added

--- SDL/SDL2-2.0.3/include/SDL_rect.h
+++ SDL/SDL2-2.0.3/include/SDL_rect.h
@@ -77,6 +77,8 @@ typedef struct SDL_Rect
 {
     int x, y;
     int w, h;
+    SDL_Rect( ) : x( 0 ), y( 0 ), w( 0 ), h( 0 ) { }
+    SDL_Rect( int _x, int _y, int _w, int _h ) : x( _x ), y( _y ), w( _w ), h( _h ) { }
 } SDL_Rect;
 
 /**
